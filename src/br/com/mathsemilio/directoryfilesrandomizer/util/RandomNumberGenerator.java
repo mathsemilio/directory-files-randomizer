1f9c2d5b035981fd81bae915e5042dfc1928fe75
Matt Menezes <mattmenezes@mattmenezes.home>
2021-04-17 15:58:06 -0300

    Added license header to all Java source files; Renamed currentIndex field to mCurrentIndex in RandomNumberGenerator.java file

--- src/br/com/mathsemilio/directoryfilesrandomizer/util/RandomNumberGenerator.java
+++ src/br/com/mathsemilio/directoryfilesrandomizer/util/RandomNumberGenerator.java
@@ -1,3 +1,18 @@
+/*
+   Copyright 2021 Matt Menezes
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
 package br.com.mathsemilio.directoryfilesrandomizer.util;
 
 import java.util.ArrayList;
@@ -7,7 +22,7 @@ public class RandomNumberGenerator {
 
     private final ArrayList<Integer> mUniqueNumbers = new ArrayList<>();
 
-    private int currentIndex = 0;
+    private int mCurrentIndex = 0;
 
     public RandomNumberGenerator(int upperBound) {
         generateRandomNumbers(upperBound);
@@ -24,8 +39,8 @@ public class RandomNumberGenerator {
         int randomNumber = 0;
 
         if (!mUniqueNumbers.isEmpty()) {
-            randomNumber = mUniqueNumbers.get(currentIndex);
-            ++currentIndex;
+            randomNumber = mUniqueNumbers.get(mCurrentIndex);
+            ++mCurrentIndex;
         }
 
         return randomNumber;
